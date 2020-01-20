9e795419b72a99067bebb5ad76bb3efb4fde5c56
srikar-muthyala <muthyala.srikar20@gmail.com>
2020-01-20 13:58:17 +0530

    bootstrapped signup page

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+  var email = req.body.email;
+  var fname = req.body.first;
+  var lname = req.body.second;
+  console.log(fname,email,lname);
+})
+
 app.listen(3000,function(){
   console.log("Server started on port 3000!");
 });
